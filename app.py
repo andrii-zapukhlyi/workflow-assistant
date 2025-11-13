fd9d6b3d65de1b23e7f085eb223f9fd5fd0f3735
Andrii Zapukhlyi <andriy.zapukhlyi@gmail.com>
2025-11-13 14:07:12 +0100

    Build ChromaDB database and implement retriever with metadata filtering based on employee department

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from db.crud import get_employee_by_email
 from db.db_auth import SessionLocal
+from rag.retriever import retrieve_similar_documents
 
-db = SessionLocal()
+def main():
 
-email = input("Enter your email to show an available documentation: ")
-
-try:
+    db = SessionLocal()
+    email = input("Enter employee email: ")
+    query = input("Enter your query: ")
     employee = get_employee_by_email(db, email)
-except ValueError as e:
-    print(e)
-    exit(1)
+    docs = retrieve_similar_documents(query, space_key=employee.department, k=2)
+
+if __name__ == "__main__":
+    main()
