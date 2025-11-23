253bedeefe7a349ead66d88ddf6bfac8bd7b73d8
Andrii Zapukhlyi <andriy.zapukhlyi@gmail.com>
2025-11-23 20:59:27 +0100

    Implement user query handling in qa_agent, refactor app.py to utilize new functionality, and update llm_client.py for improved response processing

--- rag/retriever.py
+++ rag/retriever.py
@@ -5,7 +5,7 @@ from rag.confluence_client import get_available_titles, get_public_titles
 def get_retriever(space_key: str, k: int = 2) -> VectorStoreRetriever:
     vectordb = Chroma(
         collection_name="confluence_docs",
-        persist_directory="../chroma_db"
+        persist_directory="chroma_db"
     )
 
     allowed_pages = [page["title"] for page in get_available_titles(space_key)]
@@ -14,7 +14,7 @@ def get_retriever(space_key: str, k: int = 2) -> VectorStoreRetriever:
     retriever = vectordb.as_retriever(
         search_kwargs={
             "k": k,
-            "filter": {"page_title": {"$in": allowed_pages}}
+            "filter": {"page_title": {"$in": allowed_pages}},
         }
     )
     return retriever
